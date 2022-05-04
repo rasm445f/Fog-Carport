13c464d7f88299281c2ac1f98131ef65c5ecd88a
Christoffer Nielsen <cph-cn332@cphbusiness.dk>
2022-05-04 08:55:09 +0200

    added our database in ConnectionPool

--- src/main/java/dat/startcode/model/persistence/ConnectionPool.java
+++ src/main/java/dat/startcode/model/persistence/ConnectionPool.java
@@ -11,9 +11,9 @@ import java.util.logging.Logger;
 public class ConnectionPool
 {
     private HikariDataSource ds;
-    private static String USER = "root";
-    private static String PASSWORD = "root";
-    private static String URL = "jdbc:mysql://localhost:3306/startcode";
+    private static String USER = "cphbusiness";
+    private static String PASSWORD = "cph";
+    private static String URL = "jdbc:mysql://localhost:3306/fogcarport";
 
     public ConnectionPool()
     {
