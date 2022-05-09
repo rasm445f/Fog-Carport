6fe720ef3dd4caf861502404aa090f252ceeb809
AndersMeinicke <andersmeinicke@gmail.com>
2022-05-09 10:10:03 +0200

    CreateCarport har nu post metode + der er en ny servlet.

--- src/main/java/dat/startcode/control/CreateCarport.java
+++ src/main/java/dat/startcode/control/CreateCarport.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "createCarport", value = "/createCarport")
 public class CreateCarport extends HttpServlet {
@@ -71,6 +72,38 @@ public class CreateCarport extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        session = request.getSession();
+        try {
+            Connection connection = connectionPool.getConnection();
+            session = request.getSession();
+            List<Object> carportAtributes = new ArrayList<>();
+            int cwidth = Integer.parseInt(request.getParameter("carport_width"));
+            carportAtributes.add(cwidth);
+            int clength = Integer.parseInt(request.getParameter("carport_length"));
+            carportAtributes.add(clength);
+            String rooftype = request.getParameter("rooftype");
+            carportAtributes.add(rooftype);
+            String toolshedWidth = request.getParameter("toolshed_width");
+            String[] list;
+            if (toolshedWidth != "I don't want a toolshed") {
+                list = toolshedWidth.split(" ");
+                carportAtributes.add(list[0]);
+            } else {
+                carportAtributes.add(toolshedWidth);
+            }
+            String toolshedLength = request.getParameter("toolshed_length");
+            if (toolshedLength != "I don't want a toolshed") {
+                list = toolshedLength.split(" ");
+                carportAtributes.add(list[0]);
+            } else {
+                carportAtributes.add(toolshedLength);
+            }
+            session.setAttribute("carportAtributes", carportAtributes);
+            request.getRequestDispatcher("Calculator");
+            connection.close();
+        }
+        catch (SQLException e){
+
+        }
     }
-}
+
+    }
