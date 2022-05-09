5ede9aea222c052ae90559bfd4fa07c49fbb655a
Christoffer Nielsen <cph-cn332@cphbusiness.dk>
2022-05-09 12:11:26 +0200

    changed materialsmapper so it gets the id from the DB

--- src/main/java/dat/startcode/model/persistence/MaterialsMapper.java
+++ src/main/java/dat/startcode/model/persistence/MaterialsMapper.java
@@ -22,12 +22,13 @@ public class MaterialsMapper {
             try(PreparedStatement ps = connection.prepareStatement(sql)){
                 ResultSet rs = ps.executeQuery();
                 while (rs.next()){
+                    int materialID = rs.getInt("material_id");
                     String materialDescription = rs.getString("material_description");
                     String materialCategory = rs.getString("material_category");
                     String materialUnit = rs.getString("material_unit");
                     String materialDimension = rs.getString("material_dimension");
                     int materialPrice = rs.getInt("material_price");
-                    Materials materials = new Materials(materialDescription,materialCategory,materialUnit,materialDimension,materialPrice);
+                    Materials materials = new Materials(materialID,materialDescription,materialCategory,materialUnit,materialDimension,materialPrice);
                     materialList.add(materials);
                 }
             }
