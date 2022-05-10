3e49d4a957e6b2f7a2749d4243db8aaa7ca9dd97
AndersMeinicke <andersmeinicke@gmail.com>
2022-05-10 11:59:55 +0200

    Math

--- src/main/java/dat/startcode/model/persistence/CalculatorMapper.java
+++ src/main/java/dat/startcode/model/persistence/CalculatorMapper.java
@@ -8,27 +8,77 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CalculatorMapper {
-    List<Object> carportAtributes;
-    public CalculatorMapper(List<Object> carportAtributes){
-        this.carportAtributes = carportAtributes;
+    List<Object> carportAttributes;
+    public CalculatorMapper(List<Object> carportAttributes){
+        this.carportAttributes = carportAttributes;
     }
-    int cwidth = (int) carportAtributes.get(0);
-    int clength = (int) carportAtributes.get(1);
-    String rooftype = (String) carportAtributes.get(2);
+    int cwidth = (int) carportAttributes.get(0);
+    int clength = (int) carportAttributes.get(1);
+    String rooftype = (String) carportAttributes.get(2);
 
     MaterialsMapper materialsMapper;
     ArrayList<Materials> materialList = materialsMapper.CreateMaterials();
     ArrayList<BillOfMaterials> BOMList = new ArrayList<>();
 
     public void calculateEverything(){
+        calculateStolper();
         calculateUndersternBraederForBackAndFront();
+        calculateUndersternbraederforSides();
+        calculateOversternBraederForFront();
+        calculateOversternbraederforSides();
         calculateSpaer();
     }
     public void calculateStolper(){
-
+        int length = clength;
+        int amountOfStolper = 1;
+        int spaceFromBeginningToFirstStolpe;
+        while(length > 310){
+            amountOfStolper++;
+            length = length - 310;
+        }
+        if(amountOfStolper == 1){
+            amountOfStolper++;
+            amountOfStolper = amountOfStolper * 2;
+            spaceFromBeginningToFirstStolpe = 0;
+        }
+        else{
+            spaceFromBeginningToFirstStolpe = length/2;
+        }
     }
     public void calculateUndersternBraederForBackAndFront(){
-
+        int amountofUndersternBraeder = 1;
+        int width = cwidth;
+        while (width > 360){
+            amountofUndersternBraeder++;
+            width = width-360;
+        }
+        amountofUndersternBraeder = amountofUndersternBraeder * 2;
+    }
+    public void calculateUndersternbraederforSides(){
+        int amountofUndersternSider = 1;
+        int length = clength;
+        while (length > 540){
+            amountofUndersternSider++;
+            length = length-540;
+        }
+        amountofUndersternSider = amountofUndersternSider * 2;
+    }
+    public void calculateOversternBraederForFront(){
+        int amountofOversternFront = 1;
+        int width = cwidth;
+        while (width > 360){
+            amountofOversternFront++;
+            width = width-360;
+        }
+    }
+    public void calculateOversternbraederforSides(){
+        int amountofOversternSider = 1;
+        int length = clength;
+        while (length > 540){
+            amountofOversternSider++;
+            length = length-540;
+        }
+        amountofOversternSider = amountofOversternSider * 2;
     }
     public void calculateSpaer(){
 
@@ -45,4 +95,5 @@ public class CalculatorMapper {
         }
 
     }
+
 }
