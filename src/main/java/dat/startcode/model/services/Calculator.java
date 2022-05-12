7f317652d4e42a6dea7679be27ae17b8a4345c46
AndersMeinicke <andersmeinicke@gmail.com>
2022-05-12 09:51:28 +0200

    Math

--- src/main/java/dat/startcode/model/services/Calculator.java
+++ src/main/java/dat/startcode/model/services/Calculator.java
@@ -9,125 +9,226 @@ import java.util.List;
 
 public class Calculator {
     List<Object> carportAttributes;
-    public Calculator(List<Object> carportAttributes){
+
+    public Calculator(List<Object> carportAttributes) {
         this.carportAttributes = carportAttributes;
+        cwidth = (int) carportAttributes.get(0);
+        clength = (int) carportAttributes.get(1);
+        rooftype = (String) carportAttributes.get(2);
     }
-    int cwidth = (int) carportAttributes.get(0);
-    int clength = (int) carportAttributes.get(1);
-    String rooftype = (String) carportAttributes.get(2);
+
+    int cwidth;
+    int clength;
+    String rooftype;
 
     MaterialsMapper materialsMapper;
     ArrayList<Materials> materialList = materialsMapper.CreateMaterials();
     ArrayList<BillOfMaterials> BOMList = new ArrayList<>();
 
-    public void calculateEverything(){
-        calculateStolper();
-        calculateUndersternBraederForBackAndFront();
-        calculateUndersternbraederforSides();
-        calculateOversternBraederForFront();
-        calculateOversternbraederforSides();
-        calculateSpaer();
-        calculateVandbraetForSides();
-        calculateVandbraetForFront();
-        calculateRoofPlates();
+    public void calculateEverything() {
+        BOMList.add(calculateStolper());
+        BOMList.add(calculateUndersternBraederForBackAndFront());
+        BOMList.add(calculateUndersternbraederforSides());
+        BOMList.add(calculateOversternBraederForFront());
+        BOMList.add(calculateOversternbraederforSides());
+        BOMList.add(calculateVandbraetForSides());
+        BOMList.add(calculateVandbraetForFront());
+        BOMList.add(calculateSpaer());
+        BOMList.add(calculateRoofPlates());
+        BOMList.add(calculateRoofPlatesSmall());
     }
-    public void calculateStolper(){
+
+    public BillOfMaterials calculateStolper() {
+        String prerequesitOne = "97x97mm. trykimp. Stolpe";
         int length = clength;
+        int materialLength = 300;
         int amountOfStolper = 1;
         int spaceFromBeginningToFirstStolpe;
-        while(length > 310){
+        while (length > 310) {
             amountOfStolper++;
             length = length - 310;
         }
-        if(amountOfStolper == 1){
+        if (amountOfStolper == 1) {
             amountOfStolper++;
             amountOfStolper = amountOfStolper * 2;
             spaceFromBeginningToFirstStolpe = 0;
-        }
-        else{
+        } else {
             amountOfStolper = amountOfStolper * 2;
-            spaceFromBeginningToFirstStolpe = length/2;
+            spaceFromBeginningToFirstStolpe = length / 2;
         }
+        for (Materials materials : materialList) {
+            if (materials.getMaterialDescription() == prerequesitOne && materials.getMaterialLength() == materialLength) {
+                BillOfMaterials billOfMaterials = new BillOfMaterials(amountOfStolper, materials.getMaterialID(), "Stolper nedgraves 90cm. i jord");
+                return billOfMaterials;
+            }
+        }
+        return null;
     }
-    public void calculateUndersternBraederForBackAndFront(){
+
+    public BillOfMaterials calculateUndersternBraederForBackAndFront() {
+        String prerequesitOne = "25x200mm. trykimp. Brædt";
         int amountofUndersternBraeder = 1;
         int width = cwidth;
-        while (width > 360){
+        int materialLength = 360;
+        while (width > 360) {
             amountofUndersternBraeder++;
-            width = width-360;
+            width = width - 360;
         }
         amountofUndersternBraeder = amountofUndersternBraeder * 2;
+        for (Materials materials : materialList) {
+            if (materials.getMaterialDescription() == prerequesitOne && materials.getMaterialLength() == materialLength) {
+                BillOfMaterials billOfMaterials = new BillOfMaterials(amountofUndersternBraeder, materials.getMaterialID(), "understernbrædder til for og bag ende");
+                return billOfMaterials;
+            }
+        }
+        return null;
     }
-    public void calculateUndersternbraederforSides(){
+
+    public BillOfMaterials calculateUndersternbraederforSides() {
+        String prerequesitOne = "25x200mm. trykimp. Brædt";
         int amountofUndersternSider = 1;
+        int materialLength = 540;
         int length = clength;
-        while (length > 540){
+        while (length > 540) {
             amountofUndersternSider++;
-            length = length-540;
+            length = length - 540;
         }
         amountofUndersternSider = amountofUndersternSider * 2;
+        for (Materials materials : materialList) {
+            if (materials.getMaterialDescription() == prerequesitOne && materials.getMaterialLength() == materialLength) {
+                BillOfMaterials billOfMaterials = new BillOfMaterials(amountofUndersternSider, materials.getMaterialID(), "understernbrædder til siderne");
+                return billOfMaterials;
+            }
+        }
+        return null;
     }
-    public void calculateOversternBraederForFront(){
+
+    public BillOfMaterials calculateOversternBraederForFront() {
+        String prerequesitOne = "25x125mm. trykimp. Brædt";
         int amountofOversternFront = 1;
         int width = cwidth;
-        while (width > 360){
+        int materialLength = 600;
+        while (width > 360) {
             amountofOversternFront++;
-            width = width-360;
+            width = width - 360;
         }
+        for (Materials materials : materialList) {
+            if (materials.getMaterialDescription() == prerequesitOne && materials.getMaterialLength() == materialLength) {
+                BillOfMaterials billOfMaterials = new BillOfMaterials(amountofOversternFront, materials.getMaterialID(), "toversternsbrædder til forende");
+                return billOfMaterials;
+            }
+        }
+        return null;
     }
-    public void calculateOversternbraederforSides(){
+
+    public BillOfMaterials calculateOversternbraederforSides() {
+        String prerequesitOne = "25x125mm. trykimp. Brædt";
         int amountofOversternSider = 1;
+        int materialLength = 540;
         int length = clength;
-        while (length > 540){
+        while (length > 540) {
             amountofOversternSider++;
-            length = length-540;
+            length = length - 540;
         }
         amountofOversternSider = amountofOversternSider * 2;
+        for (Materials materials : materialList) {
+            if (materials.getMaterialDescription() == prerequesitOne && materials.getMaterialLength() == materialLength) {
+                BillOfMaterials billOfMaterials = new BillOfMaterials(amountofOversternSider, materials.getMaterialID(), "oversternbrædder til siderne");
+                return billOfMaterials;
+            }
+        }
+        return null;
     }
-    public void calculateVandbraetForSides(){
+
+    public BillOfMaterials calculateVandbraetForSides() {
+        String prerequesitOne = "19x100 mm. trykimp. Brædt";
         int amountofVandbraetSider = 1;
+        int materialLength = 540;
         int length = clength;
-        while (length > 540){
+        while (length > 540) {
             amountofVandbraetSider++;
-            length = length-540;
+            length = length - 540;
         }
         amountofVandbraetSider = amountofVandbraetSider * 2;
-    }
-    public void calculateVandbraetForFront(){
-        int amountofVandbraetFront = 1;
-        int width = cwidth;
-        while (width > 360){
-            amountofVandbraetFront++;
-            width = width-360;
+        for (Materials materials : materialList) {
+            if (materials.getMaterialDescription() == prerequesitOne && materials.getMaterialLength() == materialLength) {
+                BillOfMaterials billOfMaterials = new BillOfMaterials(amountofVandbraetSider, materials.getMaterialID(), "vandbræt på stern i sider");
+                return billOfMaterials;
+            }
         }
+        return null;
     }
-    public void calculateSpaer(){
+
+    public BillOfMaterials calculateVandbraetForFront() {
+        String prerequesitOne = "19x100 mm. trykimp. Brædt";
+        int amountofVandbraetFront = 1;
+        int materialLength = 360;
+        int width = cwidth;
+        while (width > 360) {
+            amountofVandbraetFront++;
+            width = width - 360;
+        }
+        for (Materials materials : materialList) {
+            if (materials.getMaterialDescription() == prerequesitOne && materials.getMaterialLength() == materialLength) {
+                BillOfMaterials billOfMaterials = new BillOfMaterials(amountofVandbraetFront, materials.getMaterialID(), "vandbræt på stern i forende");
+                return billOfMaterials;
+            }
+        }
+        return null;
+    }
+
+    public BillOfMaterials calculateSpaer() {
 
         int spaceBetween = 55;
-        String prerequesitOne = "spærtræ ubh.";
-        int prerequesitTwo = 600;
-        int amountOfSpaer = (int) Math.ceil(clength/spaceBetween);
+        String prerequesitOne = "45x195mm. spærtræ ubh.";
+        int materialLength = 600;
+        int amountOfSpaer = (int) Math.ceil(clength / spaceBetween);
         for (Materials materials : materialList) {
-            if(materials.getMaterialDescription() == prerequesitOne && materials.getMaterialLength() == prerequesitTwo){
-                BillOfMaterials billOfMaterials = new BillOfMaterials(amountOfSpaer,materials.getMaterialID(),"Spær,monteres på rem");
-                BOMList.add(billOfMaterials);
-                break;
+            if (materials.getMaterialDescription() == prerequesitOne && materials.getMaterialLength() == materialLength) {
+                BillOfMaterials billOfMaterials = new BillOfMaterials(amountOfSpaer, materials.getMaterialID(), "Spær,monteres på rem");
+                return billOfMaterials;
             }
         }
 
-    }
-    public void calculateRoofPlates(){
-        int amountOfRoofPlates = 1;
-        int width = cwidth;
-        int length = clength;
-        int amountOfSmallRoofPlates = 0;
-        while (width > 109){
-            amountOfRoofPlates++;
-            width = width-109;
-        }
-        if(length > 600){
-            amountOfSmallRoofPlates = amountOfRoofPlates;
-        }
+        return null;
     }
 
+    public BillOfMaterials calculateRoofPlates() {
+        int amountOfRoofPlates = 1;
+        String prerequesitOne = "Plastmo Ecolite blåtonet";
+        int materialLength = 600;
+        int width = cwidth;
+        while (width > 109) {
+            amountOfRoofPlates++;
+            width = width - 109;
+        }
+        for (Materials materials : materialList) {
+            if (materials.getMaterialDescription() == prerequesitOne && materials.getMaterialLength() == materialLength) {
+                BillOfMaterials billOfMaterials = new BillOfMaterials(amountOfRoofPlates, materials.getMaterialID(), "tagplader monteres på spær");
+                return billOfMaterials;
+            }
+        }
+        return null;
+    }
+    public BillOfMaterials calculateRoofPlatesSmall() {
+        int amountOfRoofPlates = 1;
+        String prerequesitOne = "Plastmo Ecolite blåtonet";
+        int materialLength = 360;
+        int length = clength;
+        int width = cwidth;
+        if(length > 600) {
+            while (width > 109) {
+                amountOfRoofPlates++;
+                width = width - 109;
+            }
+            for (Materials materials : materialList) {
+                if (materials.getMaterialDescription() == prerequesitOne && materials.getMaterialLength() == materialLength) {
+                    BillOfMaterials billOfMaterials = new BillOfMaterials(amountOfRoofPlates, materials.getMaterialID(), "tagplader monteres på spær");
+                    return billOfMaterials;
+                }
+            }
+        }
+        return null;
+    }
 }
+
